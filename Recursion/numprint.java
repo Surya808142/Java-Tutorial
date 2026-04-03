72ba83862eb52c0a7851f83600665bfcd1c64e78
Surya <suryaprakash122019@gmail.com>
2026-04-03 10:58:24 +0530

    code

--- Recursion/numprint.java
+++ Recursion/numprint.java
@@ -1,10 +1,18 @@
 package Recursion;
 
-public class numprint {
+import java.util.*;
+public class numprint{
     public static void printnum(int n){
-
+        if(n==0) return;
+        System.out.println(n);
+        printnum(n-1);
     }
     public static void main(String args[]){
-
+        System.out.print("Enter the value of n : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printnum(n);
+        sc.close();
     }
 }
+
